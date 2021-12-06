e062fa712a789f69560817dacd2c2839f685b932
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2021-12-06 13:50:35 +0000

    Tidy up.

--- src/main/scala/days/day6/LanternFish.scala
+++ src/main/scala/days/day6/LanternFish.scala
@@ -15,22 +15,14 @@ object LanternFish {
 
   def run(file: String, numberOfDays: Int): Long = {
     val input = parseInput(stringFileReader(file))
-    val map = simulateDays(getMap(input), numberOfDays)
-    map.values.sum
+    simulateDays(getMap(input), numberOfDays).values.sum
   }
 
-  def parseInput(input: Seq[String]): Seq[Int] = {
-    input
-      .head
-      .split(",")
-      .map(_.toInt)
-  }
+  def parseInput(input: Seq[String]): Seq[Int] =
+    input.head.split(",").map(_.toInt)
 
-  def getMap(input: Seq[Int]): Map[Int, Long] = {
-    input
-      .groupBy(identity)
-      .map(f => (f._1, f._2.length))
-  }
+  def getMap(input: Seq[Int]): Map[Int, Long] =
+    input.groupBy(identity).map(f => (f._1, f._2.length))
 
   def simulateDays(map: Map[Int, Long], numberOfDays: Int): Map[Int, Long] = {
     @tailrec
@@ -40,23 +32,17 @@ object LanternFish {
         case _ => rec(simulateDay(acc), daysRemaining - 1)
       }
     }
-
     rec(map, numberOfDays)
   }
 
   def simulateDay(map: Map[Int, Long]): Map[Int, Long] = {
     def combineMaps(a: Map[Int, Long], b: Map[Int, Long]): Map[Int, Long] = {
-      a ++ b.map {
-        case (k, v) => k -> (v + a.getOrElse(k, 0L))
-      }
+      a ++ b.map { case (k, v) => k -> (v + a.getOrElse(k, 0L)) }
     }
 
     combineMaps(
-      map.map(f => f.copy(_1 = f._1 - 1)),
-      map.get(0) match {
-        case Some(count) => Map(6 -> count) ++ Map(8 -> count)
-        case None => Map()
-      }
-    ).removed(-1)
+      map.map(f => f.copy(_1 = f._1 - 1)).removed(-1),
+      map.get(0).fold(Map[Int, Long]())(count => Map(6 -> count) ++ Map(8 -> count))
+    )
   }
 }
