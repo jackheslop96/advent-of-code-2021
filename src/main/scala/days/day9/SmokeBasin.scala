c4ceb41aa4d523d426227b622c174a9aab7d733e
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2021-12-09 09:39:51 +0000

    Day 9 part 2.

--- src/main/scala/days/day9/SmokeBasin.scala
+++ src/main/scala/days/day9/SmokeBasin.scala
@@ -4,29 +4,49 @@ import utils.FileReader.fileReader
 
 object SmokeBasin {
 
+  type Matrix = Array[Array[Int]]
+
+  case class Coordinate(x: Int, y: Int)
+  case class Direction(x: Int, y: Int)
+  case class Location(coordinate: Coordinate, value: Int)
+
   def run(): Unit = {
     val file = "/day-9-input.txt"
-    println(s"Day 9 part 1 result: ${run(file)}")
+    println(s"Day 9 part 1 result: ${part1(file)}")
+    println(s"Day 9 part 2 result: ${part2(file)}")
     println()
   }
 
-  def run(file: String): Int = {
+  def part1(file: String): Int = {
     val matrix = initialiseMatrix(file)
-    var counter = 0
-    for (x <- matrix.head.indices; y <- matrix.indices) {
-      val value = matrix(y)(x)
-      if (isLessThanAdjacents(matrix, value, x, y)) counter = counter + value + 1
-    }
-    counter
+    findBasins(matrix).map(_.value + 1).sum
   }
 
-  def initialiseMatrix(file: String): Array[Array[Int]] = {
+  def part2(file: String): Int = {
+    val matrix = initialiseMatrix(file)
+    findBasins(matrix)
+      .map(lowPoint => calculateSizeOfBasin(matrix, lowPoint.coordinate))
+      .sortWith(_ > _)
+      .take(3)
+      .product
+  }
+
+  def initialiseMatrix(file: String): Matrix = {
     fileReader(file)
       .map(_.map(_.toString.toInt).toArray)
       .toArray
   }
 
-  private def isLessThanAdjacents(matrix: Array[Array[Int]], value: Int, x: Int, y: Int): Boolean = {
+  private def findBasins(matrix: Matrix): Seq[Location] = {
+    var basins: Seq[Location] = Nil
+    for (x <- matrix.head.indices; y <- matrix.indices) {
+      val value = matrix(y)(x)
+      if (isLessThanAdjacents(matrix, value, x, y)) basins = basins :+ Location(Coordinate(x, y), value)
+    }
+    basins
+  }
+
+  private def isLessThanAdjacents(matrix: Matrix, value: Int, x: Int, y: Int): Boolean = {
     def isLessThanAdjacent(x: Int, y: Int): Boolean =
       try {
         value < matrix(y)(x)
@@ -38,6 +58,36 @@ object SmokeBasin {
       isLessThanAdjacent(x + 1, y) &&
       isLessThanAdjacent(x, y + 1) &&
       isLessThanAdjacent(x - 1, y)
+  }
+
+  def calculateSizeOfBasin(matrix: Matrix, lowPoint: Coordinate): Int = {
+    val directions = Seq(
+      Direction(0, -1), // up
+      Direction(1, 0),  // right
+      Direction(0, 1),  // down
+      Direction(-1, 0)  // left
+    )
+
+    def rec(ds: Seq[Direction], coordinate: Coordinate, checkedCoordinates: Seq[Coordinate], acc: Int): (Int, Seq[Coordinate]) = {
+      ds match {
+        case Nil => (acc, checkedCoordinates :+ coordinate)
+        case head :: tail =>
+          try {
+            val newCoordinate = Coordinate(coordinate.x + head.x, coordinate.y + head.y)
+            if (matrix(newCoordinate.y)(newCoordinate.x) < 9 && !checkedCoordinates.contains(newCoordinate)) {
+              val res = rec(directions, newCoordinate, checkedCoordinates :+ coordinate, acc + 1)
+              rec(tail, coordinate, res._2, res._1)
+            } else {
+              rec(tail, coordinate, checkedCoordinates, acc)
+            }
+          } catch {
+            case _: ArrayIndexOutOfBoundsException => rec(tail, coordinate, checkedCoordinates, acc)
+          }
+      }
+    }
+
+    // already know that the low point is part of the basin
+    rec(directions, lowPoint, Seq(lowPoint), 1)._1
   }
 
 }
--- src/test/scala/days/day9/SmokeBasinSpec.scala
+++ src/test/scala/days/day9/SmokeBasinSpec.scala
@@ -7,27 +7,63 @@ class SmokeBasinSpec extends AnyFreeSpec {
 
   private val file = "/day-9-test-input.txt"
 
+  private val matrix: Matrix = Array(
+    Array(2, 1, 9, 9, 9, 4, 3, 2, 1, 0),
+    Array(3, 9, 8, 7, 8, 9, 4, 9, 2, 1),
+    Array(9, 8, 5, 6, 7, 8, 9, 8, 9, 2),
+    Array(8, 7, 6, 7, 8, 9, 6, 7, 8, 9),
+    Array(9, 8, 9, 9, 9, 6, 5, 6, 7, 8)
+  )
+
   "initialiseMatrix" - {
     "must return grid of all the separate values" - {
       "when given the example input" in {
         val result = initialiseMatrix(file)
-        val expectedResult = Array(
-          Array(2, 1, 9, 9, 9, 4, 3, 2, 1, 0),
-          Array(3, 9, 8, 7, 8, 9, 4, 9, 2, 1),
-          Array(9, 8, 5, 6, 7, 8, 9, 8, 9, 2),
-          Array(8, 7, 6, 7, 8, 9, 6, 7, 8, 9),
-          Array(9, 8, 9, 9, 9, 6, 5, 6, 7, 8)
-        )
-        assertResult(expectedResult)(result)
+        assertResult(matrix)(result)
       }
     }
   }
 
-  "run" - {
+  "part1" - {
     "must return 15" - {
       "when given example input" in {
-        val result = SmokeBasin.run(file)
+        val result = SmokeBasin.part1(file)
         assert(result == 15)
+      }
+    }
+  }
+
+  "calculateSizeOfBasin" - {
+    "must return 3 for top-left basin in example input" in {
+      val lowPoint = Coordinate(x = 1, y = 0)
+      val result = calculateSizeOfBasin(matrix, lowPoint)
+      assert(result == 3)
+    }
+
+    "must return 9 for top-right basin in example input" in {
+      val lowPoint = Coordinate(x = 9, y = 0)
+      val result = calculateSizeOfBasin(matrix, lowPoint)
+      assert(result == 9)
+    }
+
+    "must return 14 for middle basin in example input" in {
+      val lowPoint = Coordinate(x = 2, y = 2)
+      val result = calculateSizeOfBasin(matrix, lowPoint)
+      assert(result == 14)
+    }
+
+    "must return 9 for bottom-right basin in example input" in {
+      val lowPoint = Coordinate(x = 6, y = 4)
+      val result = calculateSizeOfBasin(matrix, lowPoint)
+      assert(result == 9)
+    }
+  }
+
+  "part2" - {
+    "must return 1134" - {
+      "when given example input" in {
+        val result = SmokeBasin.part2(file)
+        assert(result == 1134)
       }
     }
   }
